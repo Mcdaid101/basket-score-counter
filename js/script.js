8f917d388223ba5b91a530ae6966542a668fadc3
Mcdaid101 <matthewmcdaid1@hotmail.com>
2023-03-10 19:58:32 +0000

    added javascript code for counting mechanics

--- js/script.js
+++ js/script.js
@@ -2,10 +2,24 @@ let button1 = document.getElementById("btn1")
 let button2 = document.getElementById("btn2")
 let button3 = document.getElementById("btn3")
 let score = 0; 
+let onePoints = 0;
+let twoPoints = 0;
+let threePoints = 0;
 
-button1.addEventListener("click", addScore);
-button2.addEventListener("click", addScore2);
-button3.addEventListener("click", addScore3);
+button1.addEventListener("click", () => {
+    addScore();
+    onePointer();
+});
+
+button2.addEventListener("click", () => {
+    addScore2();
+    twoPointer();
+});
+
+button3.addEventListener("click", () => {
+    addScore3();
+    threePointer();
+});
 
 function addScore() {
     document.getElementById("score").textContent = score++;
@@ -17,4 +31,16 @@ function addScore2() {
 
 function addScore3() {
     document.getElementById("score").textContent = score +=3;
+}
+
+function onePointer() {
+    document.getElementById("one-pointer").textContent = onePoints++;
+}
+
+function twoPointer() {
+    document.getElementById("two-pointer").textContent = twoPoints++;
+}
+
+function threePointer() {
+    document.getElementById("three-pointer").textContent = threePoints++;
 }
